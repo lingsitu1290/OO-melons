c8ca253784b3f8cec9542ba4488bd18ed404635b
Hackbright Student <info@hackbrightacademy.com>
2016-07-14 17:26:28 -0700

    Refactored by passing in order_type and tax to parent.

--- melons.py
+++ melons.py
@@ -3,13 +3,14 @@
 class AbstractMelonOrder(object):
     """ Parent class for various types of melon orders. """
 
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, order_type, tax):
         """ Initialize all melon order types. """
 
         self.species = species
         self.qty = qty
+        self.order_type = order_type
+        self.tax = tax
         self.shipped = False
-
 
     def get_total(self):
         """ Calculates the total price of melon order. """
@@ -35,11 +36,11 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
     def __init__(self, species, qty):
         """Initialize melon order attributes"""
-        super(DomesticMelonOrder, self).__init__(species, qty)
-
-        self.order_type = "domestic"
-        self.tax = 0.08
-
+        super(DomesticMelonOrder, self).__init__(species=species, 
+                                                 qty=qty,
+                                                 order_type='domestic',
+                                                 tax=0.08)
+        
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
@@ -47,14 +48,20 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
 
-        super(InternationalMelonOrder, self).__init__(species, qty)
+        super(InternationalMelonOrder, self).__init__(species=species, 
+                                                      qty=qty, 
+                                                      order_type="international",
+                                                      tax=0.17)
 
         self.country_code = country_code
-        self.order_type = "international"
-        self.tax = 0.17
 
 
     def get_total(self):
+        """ Get the total of an international order 
+        and adds a surcharge for not reaching a minimum number of melons.  
+
+        """
+
         total = super(InternationalMelonOrder, self).get_total()
 
         if self.qty < 10:
@@ -68,3 +75,21 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
         return self.country_code
 
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """ Melon orders for the Government. """
+
+    def __init__(self, species, qty):
+        """ Initialize melon order for Government. """
+
+        super(GovernmentMelonOrder, self).__init__(species=species,
+                                                    qty=qty,
+                                                    order_type="government",
+                                                    tax=0)
+
+        self.passed_inspection = False 
+
+    def mark_inspection(self, passed):
+        """ Update order inspection status. """
+
+        self.passed_inspection = passed
